adca282fca5789bce9f1a058b4d0bb0d61e0e44a
Sarmanmalae <nikitin230905@gmail.com>
2022-03-11 19:14:56 +0300

    login commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, login_required, logout_user
 from werkzeug.utils import redirect
 
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
+from data.departments import Department
+from forms.login import LoginForm
 from forms.register import RegisterForm
 
 app = Flask(__name__)
@@ -15,6 +17,7 @@ login_manager.init_app(app)
 
 def main():
     db_session.global_init("db/mars_explorer.db")
+    db_sess = db_session.create_session()
     app.run()
 
 
@@ -66,6 +69,28 @@ def reqister():
         db_sess.commit()
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
